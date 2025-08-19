9725ae04113a0e3f35ac29a2d259f6ba1e5a87cd
Manny Reyes <Del.Valle.Reyes.Emmanuel@gmail.com>
2025-08-19 15:10:32 -0500

    simplify finding factors

--- find_factors.py
+++ find_factors.py
@@ -1,16 +1,9 @@
-#returns a list of all of the numbers which are is divisible by starting from 1 and going up to the number.
+#returns a list of all of the numbers that are  divisible by the given value, starting from 1 and going up.
 def find_factors(number):
-    my_list = []
-    for i in range(1,number):
-        n = number / i
-        if n == int(n):
-            my_list.append(int(n))
-    my_list.append(1)
-    return_list = my_list[::-1]
-    return return_list
+    return sorted([number // i for i in range(1, number + 1) if number % i == 0])
 
 
-find_factors(10) # [1,2,5,10 ]
-find_factors(11) # [1,11]
-find_factors(111) # [1,3,37,111 ]
-find_factors(321421) # [1,293,1097,321421]
+print(find_factors(10)) # [1,2,5,10 ]
+print(find_factors(11)) # [1,11]
+print(find_factors(111)) # [1,3,37,111 ]
+print(find_factors(321421)) # [1,293,1097,321421]
