bd15819a9b8edfff848f471b26118b2f908620a0
Manny Reyes <Del.Valle.Reyes.Emmanuel@gmail.com>
2025-09-03 15:25:00 -0500

    make generator expression solution

--- find_greater_numbers.py
+++ find_greater_numbers.py
@@ -1,11 +1,6 @@
-#returns the number of times a number is followed by a larger number across the entire list. 
-def find_greater_numbers(lst):
-    count = 0
-    for i in range(len(lst)):
-        for j in range(i + 1, len(lst)):  # start at the *next* number
-            if lst[j] > lst[i]:
-                count += 1
-    return count
+# Returns the number of times a number is larger than another number across the entire list.
+def find_greater_numbers(nums):
+    return sum(1 for f in range(len(nums)) for s in range(f+1, len(nums)) if nums[f] < nums[s])
 
 print(find_greater_numbers([1,2,3])) # 3 
 print(find_greater_numbers([6,1,2,7])) # 4
