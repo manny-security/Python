fbf61791bb69b935f3c8c9b231c462b9cd426017
Manny Reyes <Del.Valle.Reyes.Emmanuel@gmail.com>
2025-09-10 16:00:46 -0500

    enumerate practice

--- find_the_duplicate.py
+++ find_the_duplicate.py
@@ -1,12 +1,14 @@
-#The function returns the number which is a duplicate or None if there are no duplicates.
+#returns the number that is a duplicate or None if there are no duplicates.
 def find_the_duplicate(number_list):
-    count = 0
-    for a in number_list:
-        count += 1
-        if a in number_list[count:]:
-            return a             
-        
+    duplicates = [n for i, n in enumerate(number_list) if n in number_list[i+1:]] 
 
-find_the_duplicate([1,2,1,4,3,12]) # 1
-find_the_duplicate([6,1,9,5,3,4,9]) # 9
-find_the_duplicate([2,1,3,4]) # None
+    if duplicates:
+        return duplicates[0]
+    else:
+        return None
+
+#not the most efficient but I wanted to try list comprehension solution      
+
+print(find_the_duplicate([1,2,1,4,3,12])) # 1
+print(find_the_duplicate([6,1,9,5,3,4,9])) # 9
+print(find_the_duplicate([2,1,3,4])) # None
