1d4758f3de237330583dae6e12ba6182939f0d97
Manny Reyes <Del.Valle.Reyes.Emmanuel@gmail.com>
2025-09-10 16:05:25 -0500

    Optomized using set()

--- find_the_duplicate.py
+++ find_the_duplicate.py
@@ -1,12 +1,13 @@
-#The function returns the number which is a duplicate or None if there are no duplicates.
+#returns the number that is a duplicate or None if there are no duplicates.
 def find_the_duplicate(number_list):
-    count = 0
-    for a in number_list:
-        count += 1
-        if a in number_list[count:]:
-            return a             
+    seen = set()
+    for item in number_list:
+        if item in seen:
+            return item
+        seen.add(item)
+    return None           
         
 
-find_the_duplicate([1,2,1,4,3,12]) # 1
-find_the_duplicate([6,1,9,5,3,4,9]) # 9
-find_the_duplicate([2,1,3,4]) # None
+print(find_the_duplicate([1,2,1,4,3,12])) # 1
+print(find_the_duplicate([6,1,9,5,3,4,9])) # 9
+print(find_the_duplicate([2,1,3,4])) # None
